ac3ccee8c22b26c888b0ed147224c756e76f4a4b
Ad5001 <mail@ad5001.eu>
2017-03-05 11:29:05 +0100

    Fixing bugs...

--- src/BoxOfDevs/JQD/Main.php
+++ src/BoxOfDevs/JQD/Main.php
@@ -12,18 +12,12 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\level\Position;
-use pocketmine\level\sound\Sound;
-use pocketmine\level\sound\EndermanTeleportSound;
-use pocketmine\level\sound\AnvilFallSound;
-use pocketmine\level\sound\PopSound;
-use pocketmine\level\sound\BatSound;
-use pocketmine\level\sound\BlazeShootSound;
-use pocketmine\level\sound\DoorSound;
-use pocketmine\level\sound\FizzSound;
-use pocketmine\level\sound\GhastSound;
 
 class Main extends PluginBase implements Listener {
-	
+
+	/*
+	Called when the plugin enables
+	*/
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig("config.yml");
@@ -31,28 +25,63 @@ class Main extends PluginBase implements Listener {
 		$this->getLogger()->info(C::GREEN."Enabled!");
 	}
 	
+	/*
+	Called when a player joins
+	@param     $event    \pocketmine\event\player\PlayerJoinEvent
+	@return void
+	*/
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
 		$level = $player->getLevel();
-		$sound = $this->sounds->get('Join');
-		$level->addSound(new $sound($player));
+		$level->addSound(self::buildSound($this->sounds->get('Join'), $player));
 	}
 	
+	
+	/*
+	Called when a player quits
+	@param     $event    \pocketmine\event\player\PlayerQuitEvent
+	@return void
+	*/
 	public function onQuit(PlayerQuitEvent $event){
 		$player = $event->getPlayer();
 		$level = $player->getLevel();
-		$sound = $this->sounds->get('Quit');
-		$level->addSound(new $sound($player));
+		$level->addSound(self::buildSound($this->sounds->get('Quit'), $player));
 	}
 	
+	
+	/*
+	Called when a player dies
+	@param     $event    \pocketmine\event\player\PlayerDeathEvent
+	@return void
+	*/
 	public function onDeath(PlayerDeathEvent $event){
 		$player = $event->getPlayer();
 		$level = $player->getLevel();
-		$sound = $this->sounds->get('Death');
-		$level->addSound(new $sound($player));
+		$level->addSound(self::buildSound($this->sounds->get('Death'), $player));
 	}
 	
+	/*
+	Called when the plugin disables
+	*/
 	public function onDisable(){
 		$this->getLogger()->info(C::RED."Disabled!");
+	}
+
+	/*
+	Builds a sound from it's class name.
+	@param     $sound    string
+	@param     $pos    \pocketmine\level\Position
+	@return \pocketmine\level\sound\Sound
+	*/
+	protected static function buildSound(string $sound, Position $pos) : \pocketmine\level\sound\Sound {
+		$sound = "pocketmine\\level\\sound\\" . $sound . "Sound";
+		$r = new \ReflectionMethod($sound, "__construct");
+		$sound = "\\" . $sound;
+		$params = $r->getParameters();
+		if(isset($params[1]) && $params[1]->getName() == "pitch") {
+			return new $sound($pos, 1);
+		} else {
+			return new $sound($pos);
+		}
 	}
 }
